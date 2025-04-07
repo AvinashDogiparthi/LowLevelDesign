39a87ba8eaf06e1117be5d0079da2e815683e6be
Avinash-Dev-CFG <dogiparthi.naga@amadeus.com>
2025-04-07 06:55:25 +0530

    Builder Design Pattern implementation

---
+++ LLDandDesingPatterns/src/B_DesignPatterns/K_BuilderDesignPattern/builders/EngineeringStudentBuilder.java
@@ -0,0 +1,19 @@
+package B_DesignPatterns.K_BuilderDesignPattern.builders;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EngineeringStudentBuilder extends StudentBuilder {
+
+    @Override
+    public StudentBuilder setSubjects() {
+        List<String> subs = new ArrayList<>();
+        subs.add("DSA");
+        subs.add("OS");
+        subs.add("Computer Architecture");
+        this.subjects = subs;
+        return this;
+    }
+}
+
+
